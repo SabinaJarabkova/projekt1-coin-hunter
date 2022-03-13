10b713fef7d2e2e32f7fbfa675a34125c80b4870
Sabina Jarabkova <jarabkova.sabina@gmail.com>
2022-03-13 12:05:38 +0100

    hudba funguje

--- main.js
+++ main.js
@@ -7,56 +7,6 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
-/*
-
-
-// const canvas = document.getElementById('game');
-// const ctx = canvas.getContext('2d');
-
-// let speed = 2;
-
-// let tileCount = 20;
-// let tileSize = canvas.width/tileCount - 2;
-// let headX = 10;
-// let headY = 10;
-
-// let xVelocity = 0;
-// let yVelocity = 0;
-
-// let appleX = 5;
-// let appleY = 5;
-
-// //game loop
-// function drawGame() {
-//   clearScreen();
-//   changeSnakePosition();
-//   checkAppleColision();
-//   drawApple();
-//   drawSnake();
-//   setTimeout(drawGame, 1000/speed);
-
-// }
-
-
-// function clearScreen() {
-//   ctx.fillStyle = 'black';
-//   ctx.fillRect(0,0, canvas.width, canvas.height);
-// }
-
-
-// function drawApple () {
-//   ctx.fillStyle = 'red';
-//   ctx.fillRect(appleX, appleY, tileSize, tileSize);
-// }
-
-// function checkAppleColision() {
-//   if (appleX === headX && appleY == headY) {
-//     appleX = Math.floor(Math.random() * tileCount);
-//     appleY = Math.floor(Math.random() * tileCount);
-//   }
-// }*/
-
-
 
 
 let panacek = document.getElementById('panacek');
@@ -69,6 +19,8 @@ let pohybX = 30;
 let pohybY= 30;
 
 let hudba = document.getElementById('hudba');
+let zvukMince = document.getElementById('zvukmince');
+let fanfara = document.getElementById('zvukfanfara');
 
 let mince = document.getElementById('mince');
 let minceX = 50;
@@ -85,6 +37,7 @@ function startGame() {
   vychoziPozicePanacka();
   poziceMince();
   panacekChytilMincu();
+  gameOver();
 }
 
 function poziceMince() {
@@ -104,6 +57,16 @@ function startMusic() {
   hudba.volume = 0.5;
 }
 
+function startZvukMince() {
+  zvukMince.play();
+  hudba.volume = 0.5;
+}
+
+function startFanfara() {
+  fanfara.play();
+  fanfara.volume = 0.5;
+}
+
 document.body.addEventListener('keydown', keyDown);
   
 function keyDown(event) {
@@ -117,7 +80,7 @@ function keyDown(event) {
   }
 
   //hore
-  if (event.keyCode === 38 && ((panacekY - pohybY) <= window.innerHeight)) {
+  if (event.keyCode === 38 && ((panacekY + pohybY) <= window.innerHeight)) {
     panacekY = panacekY - pohybY;
     panacekX = panacekX;
     panacek.style.top = panacekY + 'px';
@@ -145,8 +108,19 @@ function keyDown(event) {
   panacekChytilMincu();
   gameOver();
 
+  // if (panacekChytilMincu) {
+  //   let zvukMince = document.getElementById('zvukmince');
+  //   hudba.volume() = 0.1;
+  //   zvukMince.play();
+  //   zvukMince.volume() = 1;
+  // }
 
-
+  // if (gameOver) {
+  //   let fanfara = document.getElementById('zvukfanfara');
+  //   hudba.volume = 0.1;
+  //   fanfara.play();
+  //   fanfara.volume = 1;
+  // }
 }
 
 //prekrizenie panacika a mince
@@ -155,13 +129,14 @@ function panacekChytilMincu() {
   console.log(panacekY + " " + minceY);
 
   if (!( panacekX + panacekWidth < minceX || minceX + minceWidth < panacekX || panacekY + panacekHeight < minceY || minceY + minceHeight < panacekY)) {
-    document.getElementById('zvukmince').play;
     //nova pozice mince + nacitanie skore
     poziceMince();
     //score 
     score = score + 1;
-    //document.getElementById('zvukfanfara').play;
     pocetBodu.textContent = score;
+    hudba.volume = 0.1;
+    zvukMince.play();
+    zvukMince.volume = 0.8;
     console.log('kolize s minci');
   } 
   console.log('krok');
@@ -171,14 +146,15 @@ function panacekChytilMincu() {
 function vyhra() {
   console.log('Vyhravas!');
   pocetBodu.textContent = "Vyhral si!";
-  document.getElementById('zvukfanfara').play;
-
+  hudba.volume = 0.1;
+  fanfara.play();
+  fanfara.volume = 0.8;
+  alert('Vyhráváš! Ale jestli chceš, mužeš pokračovat ve hře.');
 }
 
 function gameOver() {
-  if (pocetBodu.textContent >= 5) {
+  if (pocetBodu.textContent == 5) {
     vyhra();
-    alert('Vyhráváš! Ale jestli chceš, mužeš pokračovat ve hře.');
   }
 }
 
